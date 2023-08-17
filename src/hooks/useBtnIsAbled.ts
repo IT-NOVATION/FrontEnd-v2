f93d651b70153734aa4d0d7661f5203eeeaa4331
stakbucks <dhassidu11@gmail.com>
2023-08-17 15:55:26 +0900

    feat: SignupForm 완성

--- src/hooks/useBtnIsAbled.ts
+++ src/hooks/useBtnIsAbled.ts
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import { FieldErrors, UseFormWatch } from 'react-hook-form';
-import { ILoginForm } from '@/interface/account';
+import { IAccountInfo } from '@/interface/account';
 import { ModalState } from '@/interface/accountModal';
 
 type Props = {
   watch: UseFormWatch<any>;
-  errors: FieldErrors<ILoginForm>;
+  errors: FieldErrors<IAccountInfo>;
   modalState: number;
 };
 
-export default function useBtnIsAbled<T>({ watch, errors, modalState }: Props) {
+export default function useBtnIsAbled({ watch, errors, modalState }: Props) {
   const [isAbled, setIsAbled] = useState<boolean>(false);
   const handleIsAbled = () => {
     if (modalState === ModalState.LoginForm) {
@@ -21,29 +21,29 @@ export default function useBtnIsAbled<T>({ watch, errors, modalState }: Props) {
         setIsAbled(true);
       else setIsAbled(false);
     }
-    // if (modalState === 3) {
-    //   if (
-    //     //모든 항목들이 입력됐고, 에러가 없을 때만 제출 가능
-    //     watch("email")?.length &&
-    //     watch("password")?.length &&
-    //     watch("verifyPassword")?.length &&
-    //     !errors.email?.message &&
-    //     !errors.password?.message &&
-    //     !errors.verifyPassword?.message
-    //   ) {
-    //     setIsAbled(true);
-    //   } else setIsAbled(false);
-    // }
-    // if (modalState === 4) {
-    //   if (watch("nickname")?.length && !errors.nickname?.message) {
-    //     setIsAbled(true);
-    //   } else setIsAbled(false);
-    // }
-    // if (modalState === 7) {
-    //   if (watch("email")?.length && !errors.email?.message) {
-    //     setIsAbled(true);
-    //   } else setIsAbled(false);
-    // }
+    if (modalState === 3) {
+      if (
+        //모든 항목들이 입력됐고, 에러가 없을 때만 제출 가능
+        watch('email')?.length &&
+        watch('password')?.length &&
+        watch('verifyPassword')?.length &&
+        !errors.email?.message &&
+        !errors.password?.message &&
+        !errors.verifyPassword?.message
+      ) {
+        setIsAbled(true);
+      } else setIsAbled(false);
+    }
+    if (modalState === 4) {
+      if (watch('nickname')?.length && !errors.nickname?.message) {
+        setIsAbled(true);
+      } else setIsAbled(false);
+    }
+    if (modalState === 7) {
+      if (watch('email')?.length && !errors.email?.message) {
+        setIsAbled(true);
+      } else setIsAbled(false);
+    }
   };
   return { isAbled, setIsAbled, handleIsAbled };
 }
--- src/interface/account.ts
+++ src/interface/account.ts
@@ -1,4 +1,18 @@
+export interface IAccountInfo {
+  email?: string;
+  password?: string;
+  verifyPassword?: string;
+  nickname?: string;
+  introduction?: string;
+}
+
 export interface ILoginForm {
   email: string;
   password: string;
+}
+
+export interface ISignupForm {
+  email: string;
+  password: string;
+  verifyPassword: string;
 }
