44b7da985d07c38869cdb60d2e2520e678418bc2
stakbucks <dhassidu11@gmail.com>
2023-08-19 17:48:33 +0900

    feat: 무비서치 페이지에서 무비토크 가는 링크 생성

--- src/app/movie-search/page.tsx
+++ src/app/movie-search/page.tsx
@@ -2,7 +2,9 @@ import MovieSearch from '@/components/MovieSearch/MovieSearch';
 import { IMovieSearchMovies } from '@/interface/movieSearch';
 import { getMovieSearch } from '@/service/movieSearch';
 import getQueryClient from '@/service/queryClient';
+import RightChevronIcon from '@/ui/icons/RightChevronIcon';
 import { dehydrate, useQuery, Hydrate } from '@tanstack/react-query';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default async function MovieSearchPage() {
@@ -22,7 +24,18 @@ export default async function MovieSearchPage() {
         {'\n'}
         원하는 영화가 보이지 않는다면 상단의 검색 기능을 활용해보세요.
       </p>
-      <div className="w-[100vw] h-[85px] absolute top-[300px] left-0 bg-[#f5f5f5] "></div>
+      <div className="w-[100vw] h-[85px] absolute top-[300px] left-0 bg-[#f5f5f5] flex justify-center items-center">
+        <div className="w-[900px] flex items-center">
+          <span className="text-title5 text-theme-lightBlack mr-[59px]">
+            다른 유저들이 작성한 리뷰가 궁금하다면?
+          </span>
+          <Link href="/movie-talk">
+            <p className="text-title5 text-theme-main flex gap-[2px] items-center">
+              무비토크 가기 <RightChevronIcon />
+            </p>
+          </Link>
+        </div>
+      </div>
       <Hydrate state={dehydratedState}>
         <MovieSearch />
       </Hydrate>
