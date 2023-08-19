973b3e5761254f00483c78f987a6f7322c9f65ec
stakbucks <dhassidu11@gmail.com>
2023-08-19 16:18:00 +0900

    feat: useIntersectionObserver 훅으로 무한스크롤 감지 로직 분리

--- src/components/MovieSearch/MovieSearch.tsx
+++ src/components/MovieSearch/MovieSearch.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { IMovieSearchMovies } from '@/interface/movieSearch';
 import { getMovieSearchReviewOrder } from '@/service/movieSearch';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { useRef } from 'react';
 
 export default function MovieSearch() {
   const { data, hasNextPage, fetchNextPage, isLoading } =
@@ -16,12 +18,21 @@ export default function MovieSearch() {
         return nowPage + 1;
       },
     });
+  const getMoreMovies = useRef<HTMLDivElement>(null);
+  useIntersectionObserver({
+    root: null,
+    target: getMoreMovies,
+    onIntersect: fetchNextPage,
+    enabled: hasNextPage,
+  });
+
   return (
-    <section className="grid grid-cols-4 gap-5">
+    <section className="grid grid-cols-4 gap-5 h-[200vh]">
       {data &&
         data.pages[0].moiveSearchDtoList.map((movie) => (
           <div key={movie.movieTitle}>{movie.movieTitle}</div>
         ))}
+      <div ref={getMoreMovies} />
     </section>
   );
 }
