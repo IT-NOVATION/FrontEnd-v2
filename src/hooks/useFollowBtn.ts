da925c1087cb9f52400a7365c4e94dfe96bb40f3
stakbucks <dhassidu11@gmail.com>
2023-08-29 19:50:52 +0900

    feat: useFollowBtn 에 로그인 여부 검사에 따른 모달 출현 로직 추가

--- src/hooks/useFollowBtn.ts
+++ src/hooks/useFollowBtn.ts
@@ -1,15 +1,19 @@
 import { mutateFollow } from '@/service/follow';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useLoginState from './useLoginState';
 
 export default function useFollowBtn(userId: number, query: string[]) {
   const queryClient = useQueryClient();
-
+  const { checkAuth } = useLoginState();
   const { mutateAsync } = useMutation((data: { targetUserId: number }) =>
     mutateFollow(data)
   );
 
   const handleClick = async () => {
     const data = { targetUserId: userId };
+    if (!checkAuth()) {
+      return;
+    }
     await mutateAsync(data);
     await queryClient.invalidateQueries(query);
   };
