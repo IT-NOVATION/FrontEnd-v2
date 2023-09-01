eb5199a3b3dfa6971e514c0b886db872102c6a5a
stakbucks <dhassidu11@gmail.com>
2023-09-01 15:23:21 +0900

    fix: 리뷰메인텍스트 글자 선택 안되게 막기

--- src/components/Review/ReviewInfo/MainText/MainText.tsx
+++ src/components/Review/ReviewInfo/MainText/MainText.tsx
@@ -1,5 +1,3 @@
-import DOMPurify from 'dompurify';
-
 type Props = {
   text: string;
 };
@@ -7,9 +5,9 @@ type Props = {
 export default function MainText({ text }: Props) {
   return (
     <article
-      className="mt-[27px] mb-[90px] text-theme-lightBlack text-[16px] font-[300] leading-[32px]"
+      className="mt-[27px] mb-[90px] text-theme-lightBlack text-[16px] font-[300] leading-[32px] select-none "
       dangerouslySetInnerHTML={{
-        __html: DOMPurify.sanitize(text),
+        __html: text,
       }}
     ></article>
   );
