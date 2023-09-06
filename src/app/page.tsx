4054fc33043f6a4db4c4765f8a3392d70baa904c
stakbucks <dhassidu11@gmail.com>
2023-09-06 21:06:58 +0900

    feat: favicon 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import Banners from '@/components/Main/Banners/Banners';
 import Footer from '@/components/Main/Footer/Footer';
 import MovieTime from '@/components/Main/MovieTime/MovieTime';
 import ReviewTime from '@/components/Main/ReviewTime/ReviewTime';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: `It's Movie Time`,
+    template: `It's Movie Time | %s`,
+  },
+  description: '나만의 영화를 기록하는 시간',
+};
 
 export default function HomePage() {
   return (
