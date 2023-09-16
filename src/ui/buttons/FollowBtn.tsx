3eebe8016de3f920ae3d5edfb6250aeb583eec95
stakbucks <dhassidu11@gmail.com>
2023-09-16 19:31:07 +0900

    feat: 리뷰 작성자 정보 페이지 하단에 표시

--- src/ui/buttons/FollowBtn.tsx
+++ src/ui/buttons/FollowBtn.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function FollowBtn({ onClick }: Props) {
   return (
     <button
-      className={`w-[58px] h-[28px] rounded-[10px] border shadow-[4px_4px_4px_0px_#00000015] text-[11px] font-[300]`}
+      className={`w-[58px] h-[28px] rounded-[10px] border shadow-[4px_4px_4px_0px_#00000015] text-[11px] font-[300] bg-white`}
       onClick={onClick}
     >
       + 팔로우
